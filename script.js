fb838fa53c7d80bb679b23e291e4622546194bee
YOUNG_PRINCE <adesemowomuhammed55663@gmail.com>
2024-09-21 19:27:01 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+
+    new Typed (".tagline",{
+        strings: [
+            "Love to develop site ",
+            "love coding ",
+            "i am a young developer",
+            "i am an experienced programmer",
+        ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+            
+    })
+
+});
